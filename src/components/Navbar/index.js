b88567057a15b49285fe0b720ec808b0a105a182
Kelsey Oshiro <kelseymosh@gmail.com>
2021-02-24 10:52:25 -0700

    navbar second commit

--- src/components/Navbar/index.js
+++ src/components/Navbar/index.js
@@ -1,10 +1,32 @@
-import React from 'react'
+import React from 'react';
+import { FaBars } from 'react-icons/fa';
+import {
+    Nav,
+    NavbarContainer,
+    NavLogo,
+    MobileIcon,
+    NavMenu,
+    NavItem,
+    NavLinks
+} from './NavbarElements';
 
 const Navbar = () => {
     return (
-        <div>
-            
-        </div>
+        <>
+            <Nav> 
+                <NavbarContainer>
+                    <NavLogo to="/"><h1>ufb</h1></NavLogo>
+                    <MobileIcon>
+                        <FaBars />
+                    </MobileIcon>
+                    <NavMenu>
+                        <NavItem>
+                            <NavLinks to="about">ABOUT</NavLinks>
+                        </NavItem>
+                    </NavMenu>
+                </NavbarContainer>
+            </Nav>
+        </>
     )
 }
 
